2760a2ac47a276efdf37868b4ce12b566c30a877
Guillermo Damke <gdamke@gmail.com>
2021-10-21 11:20:50 -0300

    Commit programa 1 and 2

---
+++ programa_1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+Program 1 for the Git/Github class exercise.
+'''
+
+import math
+
+def suma(a,b):
+    pass
+
+def resta(a,b):
+    pass
+
+def multiplicacion(a,b):
+    pass
+
+def division(a,b):
+    pass
+
---
+++ programa_2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+Program 2 for the Git/Github class exercise.
+'''
+
+import math
+
+def val_absoluto(a):
+    pass
+
+def div_entero(a,b):
+    pass
+
+def div_resto(a,b):
+    pass
+
