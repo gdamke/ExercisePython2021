af32e1a5330d4ae1ab4957e69adef7cebcc4e103
Franklin Aldas <franklinaldas@hotmail.com>
2021-10-21 12:12:58 -0300

    Implementacion de la funcion suma

--- programa_1.py
+++ programa_1.py
@@ -8,7 +8,8 @@ Program 1 for the Git/Github class exercise.
 import math
 
 def suma(a,b):
-    pass
+    '''Este programa retorna el valor de la suma de a y b '''
+    return a+b
 
 def resta(a,b):
     pass
